1c0048811bf44c09782515823a12da355fca3682
mert <mertksr@users.noreply.github.com>
2023-03-20 20:25:51 +0300

    added multiselect selectbox + multiselect selectbox dependent input

--- views/islemekle.php
+++ views/islemekle.php
@@ -13,7 +13,8 @@
     <link href="../public/layouts/horizontal-light-menu/css/dark/plugins.css" rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
     <!-- Mültiselect -->
-    <link href="../public/src/plugins/src/multiselect/multi-select.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.css">
+    <script src="https://cdn.jsdelivr.net/gh/bbbootstrap/libraries@main/choices.min.js"></script>
     <!--  BEGIN CUSTOM STYLE FILE  -->
     <link rel="stylesheet" type="text/css" href="../public/src/plugins/src/stepper/bsStepper.min.css">
 
@@ -23,6 +24,80 @@
     <link rel="stylesheet" type="text/css" href="../public/src/assets/css/dark/scrollspyNav.css" />
     <link rel="stylesheet" type="text/css" href="../public/src/plugins/css/dark/stepper/custom-bsStepper.css">
     <!--  END CUSTOM STYLE FILE  -->
+    <style>
+        .multi-select-container {
+            display: inline-block;
+            position: relative;
+        }
+
+        @media only screen and (max-width: 600px) {
+            .ms-container {
+                width: 320px;
+            }
+        }
+
+        @media screen and (max-width: 900px) and (min-width: 600px) {
+            .ms-container {
+                width: 320px;
+            }
+        }
+
+        .multi-select-menu {
+            position: absolute;
+            left: 0;
+            top: 0.8em;
+            float: left;
+            min-width: 100%;
+            background: #fff;
+            margin: 1em 0;
+            padding: 0.4em 0;
+            border: 1px solid #aaa;
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+            display: none;
+        }
+
+        .multi-select-menu input {
+            margin-right: 0.3em;
+            vertical-align: 0.1em;
+        }
+
+        .multi-select-button {
+            display: inline-block;
+            font-size: 0.875em;
+            padding: 0.2em 0.6em;
+            max-width: 20em;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            vertical-align: -0.5em;
+            background-color: #fff;
+            border: 1px solid #aaa;
+            border-radius: 4px;
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+            cursor: default;
+        }
+
+        .multi-select-button:after {
+            content: "";
+            display: inline-block;
+            width: 0;
+            height: 0;
+            border-style: solid;
+            border-width: 0.4em 0.4em 0 0.4em;
+            border-color: #999 transparent transparent transparent;
+            margin-left: 0.4em;
+            vertical-align: 0.1em;
+        }
+
+        .multi-select-container--open .multi-select-menu {
+            display: block;
+        }
+
+        .multi-select-container--open .multi-select-button:after {
+            border-width: 0 0.4em 0.4em 0.4em;
+            border-color: transparent transparent #999 transparent;
+        }
+    </style>
 </head>
 
 <body class="layout-boxed" data-bs-spy="scroll" data-bs-target="#navSection" data-bs-offset="100">
@@ -69,7 +144,7 @@
                                 </div>
                                 <div class="widget-content widget-content-area">
                                     <form class="row g-3">
-                                    <div class="col-6">
+                                        <div class="col-6">
                                             <label for="inputAddress" class="form-label">Müşteri</label>
                                             <input type="text" class="form-control" id="inputAddress"
                                                 placeholder="1234 Main St">
@@ -84,20 +159,31 @@
                                                 <option>Bedirhan</option>
                                             </select>
                                         </div>
-                                        <div class="col-6">
-                                            <label for="defaultInputState" class="form-label ">Kullanılan Ürünler(Multi select?)</label>
-                                            <select form="musteriekleform" id="defaultInputState" name="ituru[]"
-                                                class="form-select">
-                                                <option selected="">Seç</option>
-                                                <option>1</option>
-                                                <option>2</option>
-                                                <option>3</option>
+
+                                        <div class="col-12">
+                                            <label for="defaultInputState" class="form-label ">Kullanılan
+                                                Ürünler</label>
+                                            <select id="choices-multiple-remove-button" name="islemyapan[]"
+                                                placeholder="Ürün Seçiniz" multiple>
+                                                <option value="50">*Alkalix Cihaz</option>
+                                                <option value="50">*Aqualine Cihaz</option>
+                                                <option value="20">*5 micron</option>
+                                                <option value="70">Tuz filt</option>
+                                                <option value="50">Akış vanası</option>
+                                                <option value="100">vana</option>
+
                                             </select>
+                                        </div>
+
+
+                                        <div class="col-12 col-lg-6 col-md-6">
+                                            <label for="inputAddress" class="form-label">İşlem Ücreti(readonly?)</label>
+                                            <input type="text" class="form-control" id="cost" name="cost">
                                         </div>
                                         <div class="col-6">
                                             <label for="defaultInputState" class="form-label ">Periyot</label>
                                             <select form="musteriekleform" id="defaultInputState" name="ituru[]"
-                                                class="form-select">
+                                                class="form-select select">
                                                 <option selected="">6</option>
                                                 <option>12</option>
                                                 <option>3</option>
@@ -106,11 +192,7 @@
                                                 <option>4</option>
                                             </select>
                                         </div>
-                                        <div class="col-6">
-                                            <label for="inputAddress" class="form-label">İşlem Ücreti (OTOMATİK GElcek)</label>
-                                            <input type="text" class="form-control" id="inputAddress"
-                                                placeholder="1234 Main St">
-                                        </div>
+
                                         <div class="col-6">
                                             <label for="inputAddress2" class="form-label">Notlar</label>
                                             <input type="text" class="form-control" id="inputAddress2"
@@ -137,8 +219,41 @@
     <!--  END CONTENT AREA  -->
     </div>
     <!-- END MAIN CONTAINER -->
-    <script src="../public/src/plugins/multiselect/jquery.multi-select.js"></script>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
+        integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="../public/src/plugins/src/multiselect/jquery.multi-select.js"></script>
+    <script>
+
+        /*Multi select get data and sum */
+        $("#choices-multiple-remove-button").on("change", function () {
+            var selValue = $("#choices-multiple-remove-button").val();
+
+            var selValue = selValue.map(function (x) {
+                return parseInt(x, 10);
+            });
+            const sum = selValue.reduce((partialSum, a) => partialSum + a, 0);
+            $("#cost").val(sum);
+        });
+        /* End Of Multi select get data and sum */
+
+        /* Multiselect Settings*/
+        $(document).ready(function () {
+
+            var multipleCancelButton = new Choices('#choices-multiple-remove-button', {
+                removeItemButton: true,
+                searchResultLimit: 5,
+                renderChoiceLimit: 8
+            });
+
+
+        });
+/* End Of Multiselect Settings*/
+
+
+
+    </script>
     <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
     <script src="../public/src/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="../public/src/plugins/src/perfect-scrollbar/perfect-scrollbar.min.js"></script>
